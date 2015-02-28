31d65b72f366c7d9098fc36636f1db2a0be73ef7
Omkar Moghe <omoghe@umich.edu>
2015-02-28 16:04:19 -0600

    Auto loads in youtube

--- duococo.js
+++ duococo.js
@@ -4,18 +4,22 @@
 
 var coco_video = chrome.extension.getURL('res/coco.mp4');;
 
-duococo();
+setInterval(function() {
+    if (!document.getElementById("coco-or-bust")) {
+        duococo();
+    }
+}, 2000);
+
 function duococo () {
     var video = document.createElement('video');
     video.src = coco_video;
     video.autoPlay = true;
-    // video.style = "width:640px !important; height:360px !important"
+    video.id = "coco-or-bust";
 
     var videos = document.getElementsByTagName('video');
     if (videos.length > 0) {
         video.className = videos[0].className;
         videos[0].parentElement.replaceChild(video, videos[0]);
         video.play();
-        // window.location.reload();
     }
 }
