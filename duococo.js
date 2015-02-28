88106e16888f9bdfcaa16a6d70d3b2ee3e56550e
Omkar Moghe <omoghe@umich.edu>
2015-02-28 13:48:19 -0600

    YEEEZUS

--- duococo.js
+++ duococo.js
@@ -2,12 +2,18 @@
 // chrome.storage.sync.get("snoop", function(data) {
 // });
 
-var coco_video = "file://res/coco.mp4";
+var coco_video = chrome.extension.getURL('res/coco.mp4');;
 
 duococo();
 function duococo () {
     var video = document.createElement('video');
     video.src = coco_video;
     video.autoPlay = true;
-    document.body.appendChild(video);
+
+    var videos = document.getElementsByTagName('video');
+    if (videos.length > 0) {
+
+        videos[0].parentElement.replaceChild(video, videos[0]);
+        video.play();
+    }
 }
